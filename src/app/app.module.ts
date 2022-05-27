5a660d3c1e4008459203aa05a02c9a656bf36f70
Alphie Shikwambana <alphie.shikwambana@neutrinos.co>
2022-05-27 14:02:24 +0200

    set up firebase complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAKG04nI00jpfPih5dlIQs2tJ5G2eHkrpA",
+  authDomain: "kasi-works.firebaseapp.com",
+  projectId: "kasi-works",
+  storageBucket: "kasi-works.appspot.com",
+  messagingSenderId: "223010154281",
+  appId: "1:223010154281:web:43c96ce3b2e5f17d137938",
+  measurementId: "G-THVRSY7ELW"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 @NgModule({
   declarations: [
