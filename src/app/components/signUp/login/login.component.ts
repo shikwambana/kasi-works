1f06f00d1745cef1273816946c800c4c46380326
Chand <cftjingaete@gmail.com>
2022-05-27 21:29:51 +0200

    login + register skeleton

--- src/app/components/signUp/login/login.component.ts
+++ src/app/components/signUp/login/login.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginUserDetails: any[]= []
 
-  ngOnInit(): void {
+  registerForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
+
+  get email() {
+    return this.registerForm.get('email');
+  }
+  get password() {
+    return this.registerForm.get('password');
   }
 
+  newUserInputs() {
+    console.warn(this.registerForm.value);
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
--- src/app/components/signUp/register/register.component.ts
+++ src/app/components/signUp/register/register.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  newUserDetails: any[]= []
+  registerForm = new FormGroup({
+    fullName: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    phoneNumber: new FormControl('', [
+      Validators.required,
+      Validators.pattern('^[0-9]*$'),
+      Validators.minLength(10),
+      Validators.maxLength(10),
+    ]),
+    password: new FormControl('', [Validators.required]),
+    confirmPassword: new FormControl('', [Validators.required]),
+  });
 
-  ngOnInit(): void {
+  registerNewUser() {
+    console.warn(this.registerForm.value);
+  }
+  get fullName() {
+    return this.registerForm.get('fullName');
+  }
+  get email() {
+    return this.registerForm.get('email');
+  }
+  get phoneNumber() {
+    return this.registerForm.get('phoneNumber');
+  }
+  get password() {
+    return this.registerForm.get('password');
+  }
+  get confirmPassword() {
+    return this.registerForm.get('confirmPassword');
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
