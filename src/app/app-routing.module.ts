fcffa93462ffd891cb5ba634a49dff9f4b14e8de
Alphie Shikwambana <alphie.shikwambana@neutrinos.co>
2022-05-27 15:17:40 +0200

    explained home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/signUp/login/login.component';
+import { RegisterComponent } from './components/signUp/register/register.component';
+import { PersonalComponent } from './components/userDetails/personal/personal.component';
+import { ProfessionalComponent } from './components/userDetails/professional/professional.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'register', component: RegisterComponent
+  },
+  {
+  path: '', component: HomeComponent, children : [
+    {
+      path: 'personal-info', component: PersonalComponent
+    },
+    {
+      path: 'professional-info', component: ProfessionalComponent
+    },
+    
+  ]
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
