dc63d954000ce2984c9e3e2f44da6b8c67d656fa
SAMIEZEEDE <siyabonga@webgooru.co.zam>
2022-06-01 11:38:36 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,9 @@ import { LoginComponent } from './components/signUp/login/login.component';
 import { RegisterComponent } from './components/signUp/register/register.component';
 import { PersonalComponent } from './components/userDetails/personal/personal.component';
 import { ProfessionalComponent } from './components/userDetails/professional/professional.component';
-<<<<<<< HEAD
-import { SkillsComponent } from './components/userDetails/skills/skills.component';
-=======
 import { HobbiesComponent } from './components/userDetails/hobbies/hobbies.component';
 import { SkillsComponent } from './components/userDetails/skills/skills.component';
 
->>>>>>> 8dbe11bb33f8b62bde6ee149686c2d638ff3488b
 
 const routes: Routes = [
   // {
@@ -21,33 +17,31 @@ const routes: Routes = [
   //   path: 'register', component: RegisterComponent
   // },
   {
-  path: '', component: HomeComponent, children : [
-    {
-      path: 'personal-info', component: PersonalComponent
-    },
-    {
-      path: 'professional-info', component: ProfessionalComponent
-    },
-    {
-<<<<<<< HEAD
-      path: 'skills', component:SkillsComponent
-    },
-=======
-      path: 'hobbies-info', component: HobbiesComponent
-    },
-    {
-      path: 'skills-info', component: SkillsComponent
-    },
-    {
-      path: 'register', component: RegisterComponent
-    },
-    {
-      path: 'login', component: LoginComponent
-    }
->>>>>>> 8dbe11bb33f8b62bde6ee149686c2d638ff3488b
-    
-  ]
-}
+    path: '', component: HomeComponent, children: [
+      {
+        path: 'personal-info', component: PersonalComponent
+      },
+      {
+        path: 'professional-info', component: ProfessionalComponent
+      },
+      {
+        path: 'skills', component: SkillsComponent
+      },
+      {
+        path: 'hobbies-info', component: HobbiesComponent
+      },
+      {
+        path: 'skills-info', component: SkillsComponent
+      },
+      {
+        path: 'register', component: RegisterComponent
+      },
+      {
+        path: 'login', component: LoginComponent
+      }
+
+    ]
+  }
 ];
 
 @NgModule({
